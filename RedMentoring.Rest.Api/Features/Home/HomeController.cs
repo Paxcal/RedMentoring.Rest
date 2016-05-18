9a572bf66d7f0ad6f0d30a10ec1ec489be5dcf93
Pascal Amey <pascal@redmentoring.com>
2016-05-18 22:31:26 +0200

    Home added

---
+++ RedMentoring.Rest.Api/Features/Home/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+
+namespace RedMentoring.Rest.Api.Features.Home
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public HttpResponseMessage Index()
+        {
+            var response = new HttpResponseMessage();
+            response.Content = new StringContent("<html><body><h1>RedMentoring.Rest.Api</h1></body></html>", Encoding.UTF8, "text/html");
+            return response;
+        }
+    }
+}
